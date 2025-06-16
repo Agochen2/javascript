1ba8c3988d93754505536a04ba9a0d3a30058ebd
angelinagochenour@gmail.com <you@example.com>
2025-06-16 11:24:21 -0500

    string lesson

---
+++ string.js
@@ -0,0 +1,44 @@
+const longString = "Hello, learner welcome to JavaScript Fundamentals"
+const anotherLongString = 'Hello, learner welcome to JavaScript Fundamentals'+
+                            'hfkajhgkjskfjasjflasdjfljahfgjhtfjhdkfh wahsdkfhkwhtf'
+
+//let alpha = "Hello";
+//let beta = 'world';
+//let number = '10';
+
+
+//let combineVariables = alpha + beta;
+//let combineVariables = alpha + number;
+
+
+//console.log(combineVariables)
+//console.log(alpha)
+//console.log(beta)
+
+const VALUE_ONE = 10;
+const VALUE_TWO = 20;
+
+//console.log ('Thirty is'+ (VALUE_ONE + VALUE_TWO)+ 'and not'+ (2*VALUE_ONE + VALUE_TWO) );
+
+//console.log ('Thirty is'+ VALUE_ONE + VALUE_TWO+ 'and not'+ 2*VALUE_ONE + VALUE_TWO );
+
+//console.log (`Thirty is ${VALUE_ONE + VALUE_TWO} and not ${2*VALUE_ONE + VALUE_TWO}`);
+
+//console.log (`${VALUE_TWO} is greater than ${VALUE_ONE}`)
+
+//console.log ('it\'s a cat')
+
+//console.log (longString.length);
+const name ="Carl"
+//console.log (name.length);
+
+//console.log(name.at(0));
+//console.log(name.at(-3));
+
+//console.log(name.includes('ar'));
+
+//console.log (name.toUpperCase());
+
+console.log ("    asjhgkhdjg   jhdjkghajdkd hdgkjdf       ".trim())
+console.log ("    asjhgkhdjg   jhdjkghajdkd hdgkjdf       ".trim().length)
+console.log ("    asjhgkhdjg   jhdjkghajdkd hdgkjdf       ".length)
