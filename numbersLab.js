f3e3cefd32c1fe88f63a0ec3f17483247621bf26
angelinagochenour@gmail.com <you@example.com>
2025-06-21 16:11:21 -0500

    number changes

---
+++ numbersLab.js
@@ -0,0 +1,21 @@
+const value1 = 34_038;
+const value2 = 45_838.8734;
+const value3 = 0b0110_0010_1011
+const value4 = 0x17_4C_0;
+const value5 = -895_612;
+
+console.log(value1);
+console.log(value2);
+console.log(value3);
+console.log(value4);
+console.log(value5);
+
+const decimal = 29;
+const hex = 0x1d;
+const binary = 0b11101;
+const octal = 0o35;
+
+console.log(decimal);
+console.log(hex);
+console.log(binary);
+console.log(octal);
